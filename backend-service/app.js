d496f81f4ce4e9b02508d87a7261d8d3dff6a01c
Oswald <orijo@falabella.com.ar>
2020-05-22 19:22:12 -0300

    testing infra

---
+++ backend-service/app.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const compression = require('compression');
+const cors = require('cors');
+
+const app = express();
+
+app.set('trust proxy', true);
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(compression());
+
+app.get(`/${process.env.COUNTRY}/v1/health/`, (req, res) => {
+  res.send(`${process.env.COUNTRY} up and running.`);
+});
+
+app.use((err, req, res, next) => { // eslint-disable-line
+  res.status(500).send({ code: 'error', message: err.message });
+});
+
+module.exports = app;
